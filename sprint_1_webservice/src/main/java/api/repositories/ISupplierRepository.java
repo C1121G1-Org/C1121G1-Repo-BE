8cc114ada403d074b8f33b39c1ae981b3e539cf2
Tran Thi Bich Ngoc <ngoctran970627@gmail.com>
2022-05-31 21:44:30 +0700

    create supplier ngocTTB

--- sprint_1_webservice/src/main/java/api/repositories/ISupplierRepository.java
+++ sprint_1_webservice/src/main/java/api/repositories/ISupplierRepository.java
@@ -2,7 +2,11 @@ package api.repositories;
 
 import api.models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ISupplierRepository extends JpaRepository<Supplier, Long> {
+
+    @Query(value = " INSERT INTO supplier ( address, email, phone_number, supplier_name, delete_flag) VALUES (?, ?, ?, ?, ?);", nativeQuery = true)
+    void createSupplier(String supplierName, String address, String phone, String email,boolean deleteFlag );
     Supplier findBySupplierName(String supplierName);
 }
