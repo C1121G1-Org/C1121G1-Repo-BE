8a9585dc03d4f324f9149cb0591ffaf0085c4517
Công Nguyễn <nguyenvancong2107@gmail.com>
2022-05-31 22:35:41 +0700

    congNV_History

--- sprint_1_webservice/src/main/java/api/models/Invoice.java
+++ sprint_1_webservice/src/main/java/api/models/Invoice.java
@@ -13,10 +13,13 @@ public class Invoice {
 
     @Column(name = "create_time", nullable = false, length = 50)
     private String createTime;
+
     @Column(name = "create_date", nullable = false, length = 50)
     private String createDate;
+
     @Column(name = "total_money", nullable = false)
     private Double totalMoney;
+
     @Column(name = "payments", nullable = false, length = 50)
     private String payments;
 
@@ -24,15 +27,12 @@ public class Invoice {
     @JoinColumn(name = "customer_id", nullable = false, referencedColumnName = "id")
     private Customer customer;
 
-//    @OneToOne
-//    @JoinColumn(name = "invoiceDetail_id", nullable = false, referencedColumnName = "id")
-//    private InvoiceDetail invoiceDetail;
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false, referencedColumnName = "id")
+    private Product product;
 
     @OneToMany(mappedBy = "invoice")
     private Set<InvoiceDetail> invoiceDetailSet;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false, referencedColumnName = "id")
-    private Product product;
 
 }
