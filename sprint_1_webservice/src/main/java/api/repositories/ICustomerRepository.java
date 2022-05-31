31e1790e699bd615775820e98432fd861343e003
hoang466a <hoangtn96@gmail.com>
2022-05-31 16:28:30 +0700

    day sprint 1 ngay 31/5/2022

--- sprint_1_webservice/src/main/java/api/repositories/ICustomerRepository.java
+++ sprint_1_webservice/src/main/java/api/repositories/ICustomerRepository.java
@@ -4,4 +4,5 @@ import api.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+        
 }
