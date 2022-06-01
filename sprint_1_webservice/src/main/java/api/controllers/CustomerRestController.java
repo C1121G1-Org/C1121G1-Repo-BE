8c40e3001f63aa8efb35e737cfcff1f5440f98f7
nguyenquoctuan110398 <nguyenquoctuan11031998@gmail.com>
2022-06-01 08:40:59 +0700

    TuanNQ - code BE purchase history and list report customer

--- sprint_1_webservice/src/main/java/api/controllers/CustomerRestController.java
+++ sprint_1_webservice/src/main/java/api/controllers/CustomerRestController.java
@@ -1,8 +1,18 @@
 package api.controllers;
 
+import api.dto.PurchaseHistoryDto;
+import api.dto.ReportCustomerDto;
 import api.services.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
@@ -14,24 +24,82 @@ public class CustomerRestController {
 
 
     @GetMapping(value = "/list")
-    public String listCustomer(){
+    public String listCustomer() {
         return null;
     }
 
     @PostMapping(value = "/create")
-    public String createCustomer(){
+    public String createCustomer() {
         return null;
     }
 
     @PatchMapping(value = "/update")
-    public String updateCustomer(){
+    public String updateCustomer() {
         return null;
     }
 
     @DeleteMapping(value = "/delete") //Nếu dùng deleteFlag thì phải dùng @PatchMapping để update lại deleteFlag
-    public String deleteCustomer(){
+    public String deleteCustomer() {
         return null;
     }
 
+    /*
+        Created by TuanNQ
+        Time: 18:00 31/05/2022
+        Function: Show all list report customer
+    */
+    @GetMapping(value = "/report-customer")
+    public ResponseEntity<List<ReportCustomerDto>> showListReportCustomer(
+            @PageableDefault Pageable pageable) {
+
+        Page<ReportCustomerDto> reportCustomerDtoPage = iCustomerService.filterAll(pageable);
+
+        if (reportCustomerDtoPage.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+
+        List<ReportCustomerDto> reportCustomerDtos = reportCustomerDtoPage.toList();
+        return new ResponseEntity<>(reportCustomerDtos, HttpStatus.OK);
+    }
+
+    /*
+        Created by TuanNQ
+        Time: 18:00 31/05/2022
+        Function: Show list of customer reports by age and gender
+    */
+    @GetMapping(value = "/report-customer-search")
+    public ResponseEntity<List<ReportCustomerDto>> showListReportCustomerSearch(
+            @PageableDefault Pageable pageable, @RequestParam Boolean gender,
+            @RequestParam String age) {
+
+        Page<ReportCustomerDto> reportCustomerDtoPage =
+                iCustomerService.filterByGenderAndAge(pageable, gender, age);
+
+        if (reportCustomerDtoPage.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+
+        List<ReportCustomerDto> reportCustomerDtos = reportCustomerDtoPage.toList();
+        return new ResponseEntity<>(reportCustomerDtos, HttpStatus.OK);
+    }
+
+    /*
+        Created by TuanNQ
+        Time: 18:15 31/05/2022
+        Function: Show detail purchase history of customer
+    */
+    @GetMapping(value = "/purchase-history/{id}")
+    public ResponseEntity<List<PurchaseHistoryDto>> showDetailPurchaseHistory(
+            @PathVariable Long id, @PageableDefault Pageable pageable) {
+
+        Page<PurchaseHistoryDto> purchaseHistoryDtoPage = iCustomerService.detailPurchaseHistory(id, pageable);
+
+        if (purchaseHistoryDtoPage.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } else {
+            List<PurchaseHistoryDto> purchaseHistoryDtos = purchaseHistoryDtoPage.toList();
+            return new ResponseEntity<>(purchaseHistoryDtos, HttpStatus.OK);
+        }
+    }
 
 }
--- sprint_1_webservice/src/main/java/api/services/impl/CustomerService.java
+++ sprint_1_webservice/src/main/java/api/services/impl/CustomerService.java
@@ -1,13 +1,51 @@
 package api.services.impl;
 
+import api.dto.PurchaseHistoryDto;
+import api.dto.ReportCustomerDto;
 import api.repositories.ICustomerRepository;
 import api.services.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService implements ICustomerService {
 
     @Autowired
     ICustomerRepository iCustomerRepository;
+
+    /*
+        Created by TuanNQ
+        Time: 18:00 31/05/2022
+        Function: Show all list report customer
+    */
+    @Override
+    public Page<ReportCustomerDto> filterAll(Pageable pageable) {
+        return iCustomerRepository.filterAll(pageable);
+    }
+
+    /*
+        Created by TuanNQ
+        Time: 18:00 31/05/2022
+        Function: Show list of customer reports by age and gender
+    */
+    @Override
+    public Page<ReportCustomerDto> filterByGenderAndAge(Pageable pageable, Boolean gender, String age) {
+        return iCustomerRepository.filterByGenderAndAge(pageable, gender, age);
+    }
+
+    /*
+        Created by TuanNQ
+        Time: 18:15 31/05/2022
+        Function: Show detail purchase history of customer
+    */
+    @Override
+    public Page<PurchaseHistoryDto> detailPurchaseHistory(Long id, Pageable pageable) {
+        return iCustomerRepository.detail(id, pageable);
+    }
+
+
 }
