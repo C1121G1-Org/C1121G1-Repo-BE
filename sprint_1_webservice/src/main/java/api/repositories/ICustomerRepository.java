077d354731e323c407a20bf598777fde2fcd8de5
TrungHieu175 <ablohaha01@gmail.com>
2022-06-01 08:43:47 +0700

    clone

--- sprint_1_webservice/src/main/java/api/repositories/ICustomerRepository.java
+++ sprint_1_webservice/src/main/java/api/repositories/ICustomerRepository.java
@@ -2,11 +2,6 @@ package api.repositories;
 
 import api.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Long> {
-    @Query(value = "select product.*, storage.quantity from product inner join storage on product.id = storage.product_id",nativeQuery = true)
-    <T> List<T> findProductUsingService(Class<T> classType);
 }
