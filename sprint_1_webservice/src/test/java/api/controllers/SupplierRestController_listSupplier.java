86ca590acfe0790e1d11abbbc9465ce392e102d5
VoCongKhoa <vocongkhoa2208@gmail.com>
2022-06-01 18:47:16 +0700

    up junit test

--- sprint_1_webservice/src/test/java/api/controllers/SupplierRestController_listSupplier.java
+++ sprint_1_webservice/src/test/java/api/controllers/SupplierRestController_listSupplier.java
@@ -12,6 +12,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/*
+    Created by khoaVC
+    Time: 15:22 01/06/2022
+    Function: create 12 test for method create Storage
+ */
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SupplierRestController_listSupplier {
@@ -19,19 +25,17 @@ public class SupplierRestController_listSupplier {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
+    //Không test trường hợp null vì đã cài đặt giá trị mặc định cho param là "" và require = false
+    //Test case thành công
     @Test
-    public void listSupplier_supplier_11() throws Exception {
-
+    public void listSupplier_11() throws Exception {
         this.mockMvc.perform(
                         MockMvcRequestBuilders
                                 .get("/api//supplier/list/"))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(jsonPath("$.totalPages").value(2))
-                .andExpect(jsonPath("$.totalElements").value(4))
+                .andExpect(jsonPath("$.totalElements").value(6))
                 .andExpect(jsonPath("$.content[2].address").value("TP Hồ Chí Minh"))
                 .andExpect(jsonPath("$.content[2].email").value("tuongvi@gmail.com"))
                 .andExpect(jsonPath("$.content[2].phone").value("0905789654"))
@@ -39,8 +43,7 @@ public class SupplierRestController_listSupplier {
     }
 
     @Test
-    public void listSupplier_supplier_8() throws Exception {
-
+    public void listSupplier_8() throws Exception {
         this.mockMvc.perform(
                         MockMvcRequestBuilders
                                 .get("/api/supplier/list/")
@@ -52,7 +55,7 @@ public class SupplierRestController_listSupplier {
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(jsonPath("$.totalPages").value(2))
-                .andExpect(jsonPath("$.totalElements").value(4))
+                .andExpect(jsonPath("$.totalElements").value(6))
                 .andExpect(jsonPath("$.content[2].address").value("TP Hồ Chí Minh"))
                 .andExpect(jsonPath("$.content[2].email").value("tuongvi@gmail.com"))
                 .andExpect(jsonPath("$.content[2].phone").value("0905789654"))
@@ -71,6 +74,23 @@ public class SupplierRestController_listSupplier {
     }
 
     @Test
+    public void listSupplier_supplier_11() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/supplier/list/")
+                                .param("su","Trung HIếu")
+                )
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.totalPages").value(1))
+                .andExpect(jsonPath("$.totalElements").value(1))
+                .andExpect(jsonPath("$.content[0].address").value("Hà Nội"))
+                .andExpect(jsonPath("$.content[0].email").value("trunghieu@gmail.com"))
+                .andExpect(jsonPath("$.content[0].phone").value("0905444111"))
+                .andExpect(jsonPath("$.content[0].supplierName").value("Công Ty Cổ Phần Trung HIếu"));
+    }
+
+    @Test
     public void listSupplier_address_9() throws Exception {
         this.mockMvc.perform(
                         MockMvcRequestBuilders
@@ -79,6 +99,23 @@ public class SupplierRestController_listSupplier {
                 )
                 .andDo(print())
                 .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void listSupplier_address_11() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/supplier/list/")
+                                .param("ad","TT Huế")
+                )
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.totalPages").value(1))
+                .andExpect(jsonPath("$.totalElements").value(2))
+                .andExpect(jsonPath("$.content[0].address").value("TT Huế"))
+                .andExpect(jsonPath("$.content[0].email").value("haivan75@gmail.com"))
+                .andExpect(jsonPath("$.content[0].phone").value("0901232262"))
+                .andExpect(jsonPath("$.content[0].supplierName").value("Công Ty Cổ Phần Hải Vân"));
     }
 
     @Test
@@ -93,6 +130,23 @@ public class SupplierRestController_listSupplier {
     }
 
     @Test
+    public void listSupplier_phone_11() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/supplier/list/")
+                                .param("ph","0905789654")
+                )
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.totalPages").value(1))
+                .andExpect(jsonPath("$.totalElements").value(1))
+                .andExpect(jsonPath("$.content[0].address").value("TP Hồ Chí Minh"))
+                .andExpect(jsonPath("$.content[0].email").value("tuongvi@gmail.com"))
+                .andExpect(jsonPath("$.content[0].phone").value("0905789654"))
+                .andExpect(jsonPath("$.content[0].supplierName").value("Công Ty TNHH May Mặt Tường Vy"));
+    }
+
+    @Test
     public void listSupplier_email_9() throws Exception {
         this.mockMvc.perform(
                         MockMvcRequestBuilders
@@ -101,5 +155,22 @@ public class SupplierRestController_listSupplier {
                 )
                 .andDo(print())
                 .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void listSupplier_email_11() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/supplier/list/")
+                                .param("em","hoanggia111@gmail.com")
+                )
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.totalPages").value(1))
+                .andExpect(jsonPath("$.totalElements").value(1))
+                .andExpect(jsonPath("$.content[0].address").value("Đà Nẵng"))
+                .andExpect(jsonPath("$.content[0].email").value("hoanggia111@gmail.com"))
+                .andExpect(jsonPath("$.content[0].phone").value("0905789777"))
+                .andExpect(jsonPath("$.content[0].supplierName").value("Công Ty TNHH Đầu Tư May Mặt Hoàng Gia"));
     }
 }
